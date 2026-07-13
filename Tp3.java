036b727a6cf94ac3034e30bdd6308d5bd723f938
Max Henry <max.henry@umontreal.ca>
2026-07-13 11:12:28 -0400

    read and store nodes and edges

--- Tp3.java
+++ Tp3.java
@@ -3,11 +3,18 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Tp3 {
 
     // Debugging flag.
     static final boolean DEBUG = true;
+
+    public enum State {
+        READ_NODES, READ_EDGES, END
+    }
 
     public static void main(String[] args) throws IOException {
         Path inputPath = Paths.get(args[0]);
@@ -15,11 +22,47 @@ public class Tp3 {
 
         // Read input.
         List<String> lines = Files.readAllLines(inputPath);
+        State state = State.READ_NODES;
+
+        // Debugging (maybe not the best data structure for this).
+        List<Node> nodes = new LinkedList<>();
+
+        // Build edges as an array first, then heapify (fixheap) for best complexity.
+        List<Edge> edges = new ArrayList<>();
+
+        // PriorityQueue<Edge> edges = new PriorityQueue<>();
 
         // Do stuff.
         for (String line : lines) {
-            if (DEBUG) {
-                System.out.println(line);
+
+            switch (state) {
+                case READ_NODES -> {
+                    if (line.equals("---")){
+                        state = State.READ_EDGES;
+                        continue;
+                    }   
+                    nodes.add(new Node(line.trim()));
+                }
+                case READ_EDGES -> {
+                    if (line.equals("---")){
+                        state = State.END;
+                        continue;
+                    }
+                    edges.add(parseEdge(line));
+                }
+                case END -> { break; }
+            }
+        }
+
+        if (DEBUG) {
+            System.out.println("\n=====\nNODES\n=====");
+            for (Node node : nodes) {
+                System.out.println(node);
+            }
+    
+            System.out.println("\n=====\nEDGES\n=====");
+            for (Edge edge: edges) {
+                System.out.println(edge);
             }
         }
 
@@ -28,5 +71,15 @@ public class Tp3 {
         // Write output.
         Files.writeString(outputPath, result);
 
+    }
+
+    public static Edge parseEdge(String line) {
+        String[] parts = line.trim().split("\\s+");
+        return new Edge(
+            parts[0],                   // label
+            new Node(parts[2]),         // start
+            new Node(parts[3]),         // end
+            Integer.parseInt(parts[4])  // weight
+        );
     }
 }
