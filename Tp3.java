0d0ca3626097b0b21c29da3fed82ca8db0d039c1
Max Henry <max.henry@umontreal.ca>
2026-07-13 09:14:06 -0400

    build skeleton to load read and write

---
+++ Tp3.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Tp3 {
+
+    // Debugging flag.
+    static final boolean DEBUG = true;
+
+    public static void main(String[] args) throws IOException {
+        Path inputPath = Paths.get(args[0]);
+        Path outputPath = Paths.get(args[1]);
+
+        // Read input.
+        List<String> lines = Files.readAllLines(inputPath);
+
+        // Do stuff.
+        if (DEBUG) {
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        }
+
+        String result = "[TO BE IMPLEMENTED]";
+
+        // Write output.
+        Files.writeString(outputPath, result);
+
+    }
+}
---
+++ run_test.sh
@@ -0,0 +1,9 @@
+# Cleanup (defensive).
+rm -f *.class 2>/dev/null
+
+# Compile and run.
+javac Tp3.java
+java Tp3 data/carte0.txt test_output.txt
+
+# Cleanup.
+rm *.class
