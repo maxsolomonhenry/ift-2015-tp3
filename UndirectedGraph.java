e753fd139b98103afef5165d21ac8aa92753a0fc
Max Henry <max.henry@umontreal.ca>
2026-07-22 20:49:02 -0400

    add lots of comments in graph class

--- UndirectedGraph.java
+++ UndirectedGraph.java
@@ -4,34 +4,43 @@ import java.util.Set;
 
 public class UndirectedGraph {
 
+    // The only property is private, completely encapsulated.
     private TreeMap<Node, TreeSet<Edge>> graph;
     
     public UndirectedGraph() {
         this.graph = new TreeMap<Node, TreeSet<Edge>>();
     }
 
+    // Add nodes one at a time.
     public void addNode(Node node) {
         graph.put(node, new TreeSet<>());
     }
 
-    public boolean hasNode(Node node) {
-        return graph.containsKey(node);
-    }
-
+    // Add a set of nodes (useful to build a skeleton MST).
     public void addNodes(Set<Node> nodes) {
         for (Node node : nodes) {
             this.addNode(node);
         }
     }
 
+    // Return nodes as set (useful to build a skeleton MST).
     public Set<Node> getNodes() {
         return graph.keySet();
     }
 
+    // Report number of nodes.
     public int getNumNodes() {
         return graph.size();
     }
 
+    // Check for nodes in graph (useful for error checking).
+    public boolean hasNode(Node node) {
+        return graph.containsKey(node);
+    }
+
+    // Add "double edge," one from each node (start to end, end to start) to 
+    // make the canonical representation of an undirected graph as an adjacency 
+    // list.
     public void addEdge(Edge edge) {
 
         if (!hasNode(edge.getStart()) || !hasNode(edge.getEnd())) {
